3c89828c000e510853c8b764a0380a76c795a903
wawanneutron <wawanneutron1331@gmail.com>
2022-07-05 00:31:33 +0700

    script js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ const header = document.querySelector(".header");
 togleNav.addEventListener("click", () => {
   header.classList.toggle("nav-open");
 });
+
+const year = document.getElementById("currentYear");
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
